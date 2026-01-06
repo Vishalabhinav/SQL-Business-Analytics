3a0aeaa26e393e8251bce1667f1f60630b1ceced
Vishalabhinav <165989533+Vishalabhinav@users.noreply.github.com>
2026-01-06 21:11:31 +0530

    Update cte_queries.sql

--- sql_queries/cte_queries.sql
+++ sql_queries/cte_queries.sql
@@ -1,1 +1,34 @@
+-- CTE 1: Calculate total spend per customer
+WITH customer_spend AS (
+    SELECT
+        customer_id,
+        SUM(price * quantity) AS total_spent
+    FROM ecommerce_sales
+    GROUP BY customer_id
+)
+
+-- Customers who spent more than average
+SELECT
+    customer_id,
+    total_spent
+FROM customer_spend
+WHERE total_spent >
+      (SELECT AVG(total_spent) FROM customer_spend);
+
+
+-- =========================================
+-- CTE 2: Monthly revenue analysis
+-- =========================================
+
+WITH monthly_sales AS (
+    SELECT
+        DATE_FORMAT(order_date, '%Y-%m') AS month,
+        SUM(price * quantity) AS revenue
+    FROM ecommerce_sales
+    GROUP BY DATE_FORMAT(order_date, '%Y-%m')
+)
+
+SELECT *
+FROM monthly_sales
+ORDER BY month;
 
