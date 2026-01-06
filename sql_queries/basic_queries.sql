dfb32fedc835b5e7c95f8066adefcadd87a18e98
Vishalabhinav <165989533+Vishalabhinav@users.noreply.github.com>
2026-01-06 21:07:59 +0530

    Update basic_queries.sql

--- sql_queries/basic_queries.sql
+++ sql_queries/basic_queries.sql
@@ -1,1 +1,23 @@
+-- View all data
+SELECT * FROM ecommerce_sales;
+
+-- Total revenue
+SELECT SUM(price * quantity) AS total_revenue
+FROM ecommerce_sales;
+
+-- Total orders per category
+SELECT category, COUNT(order_id) AS total_orders
+FROM ecommerce_sales
+GROUP BY category;
+
+-- Revenue by category
+SELECT category, SUM(price * quantity) AS revenue
+FROM ecommerce_sales
+GROUP BY category
+ORDER BY revenue DESC;
+
+-- Orders by region
+SELECT region, COUNT(order_id) AS orders
+FROM ecommerce_sales
+GROUP BY region;
 
